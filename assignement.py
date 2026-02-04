8f80ce7eb76ce3ab1a0b7444d0f233045c3bbcd5
prajwal76766 <prajwalsclk1@gmail.com>
2026-02-05 00:35:45 +0530

    saved

---
+++ assignement.py
@@ -0,0 +1,34 @@
+"""Task 1: Perform Basic Mathematical Operations
+Problem Statement: Write a Python program that does the following:
+1.  Takes two numbers as input from the user.
+2.  Performs the basic mathematical operations on these two numbers:
+o	Addition
+o	Subtraction
+o	Multiplication
+o	Division"""
+
+"""number1 = int(input("Enter the first number : "))
+number2 = int(input("Enter the second number : "))
+
+add = number1 + number2
+sub = number1 - number2
+mul = number1 * number2
+div = number1 / number2
+
+print(f"\nAddition: {add}")
+print(f"Subtraction: {sub}")
+print(f"Multiplication: {mul}")
+print(f"Division: {div}")"""
+
+"""Task 2: Create a Personalized Greeting
+Problem Statement: Write a Python program that:
+1.  Takes a user's first name and last name as input.
+2.  Concatenates the first name and last name into a full name.
+3.  Prints a personalized greeting message using the full name.
+"""
+
+name = input("Enter the first name : ")
+name1 = input("Enter the last name : ")
+
+print("Hello, ", name, name1 +"! Welcome to the Python program." )
+#print(f"Hello, {name}{name1}! Welcome to the Python program.")
